4f8dd72d9803720451d4b2824f006c27f8448650
dlscks <dlscks971026@gmail.com>
2023-04-20 15:05:53 +0900

    update

---
+++ src/sec03/exam01/BooleanExample.java
@@ -0,0 +1,17 @@
+package sec03.exam01;
+
+public class BooleanExample {
+
+	//boolean 타입 변수는 주로 두 가지 상태값에 따라 조건문과 제어문의 실행 흐름을 변경하는데 사용
+	
+	public static void main(String[] args) {
+		boolean stop = true;
+		if(stop) {
+			System.out.println("중지합니다.");
+		}else {
+			System.out.println("시작합니다.");
+		}
+
+	}
+
+}
