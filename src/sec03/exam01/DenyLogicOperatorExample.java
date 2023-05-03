7f7238123950b0a3b84655c96fac55b1598c3b4e
dlscks <dlscks971026@gmail.com>
2023-05-03 23:16:45 +0900

    논리 부정 연산자

---
+++ src/sec03/exam01/DenyLogicOperatorExample.java
@@ -0,0 +1,18 @@
+package sec03.exam01;
+
+public class DenyLogicOperatorExample {
+
+	public static void main(String[] args) {
+		boolean play = true;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+		
+
+	}
+
+}
