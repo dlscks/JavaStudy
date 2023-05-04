0133d552865be186bd1f75447d7e80613f011ab6
dlscks <dlscks971026@gmail.com>
2023-05-04 23:05:38 +0900

    char타입 산술 연산

---
+++ src/sec03/exam01/CharOperationExample.java
@@ -0,0 +1,13 @@
+package sec03.exam01;
+
+public class CharOperationExample {
+
+	public static void main(String[] args) {
+		char c1 = 'A' + 1;
+		char c2 = 'A'; 
+		System.out.println("c1:" + c1);
+		System.out.println("c2:" + c2);
+
+	}
+
+}
